c0fe097ae9c419429582fc2fbbf2c1ff80d60b6f
apprajapati9 <apprajapati@hotmail.com>
2022-01-10 22:51:24 -0500

    databinding-enabled

--- app/src/main/java/com/apprajapati/myanimations/RollDiceAnimation.kt
+++ app/src/main/java/com/apprajapati/myanimations/RollDiceAnimation.kt
@@ -9,39 +9,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.navigation.fragment.findNavController
 import com.apprajapati.myanimations.databinding.FragmentFirstBinding
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
 /**
+ *
+ * Ajay P. Prajapati ( github.com/apprajapati9)
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 
 const val DIE_INDEX_KEY = "die_index"
 const val DIE_VALUE_KEY = "die_value"
 
-class MainFragment : Fragment() {
+class RollDiceAnimation : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    private lateinit var imageViews : Array<ImageView>
+    private lateinit var imageViews: Array<ImageView>
     private val drawables = arrayOf(R.drawable.die_1,
-        R.drawable.die_2,
-        R.drawable.die_3,
-        R.drawable.die_4,
-        R.drawable.die_5,
-        R.drawable.die_6)
+            R.drawable.die_2,
+            R.drawable.die_3,
+            R.drawable.die_4,
+            R.drawable.die_5,
+            R.drawable.die_6)
 
-    private val dieHandler = object : Handler(Looper.getMainLooper()){
+    private val dieHandler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             val data = msg.data
 
-            val dieIndex =  data?.getInt(DIE_INDEX_KEY) ?: 0
+            val dieIndex = data?.getInt(DIE_INDEX_KEY) ?: 0
             val dieValue = data?.getInt(DIE_VALUE_KEY) ?: 1
 
-            imageViews.get(dieIndex).
-                setImageResource(drawables.get(dieValue -1))
+            imageViews.get(dieIndex).setImageResource(drawables.get(dieValue - 1))
         }
     }
 
@@ -63,10 +63,10 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         imageViews = arrayOf(binding.die1,
-            binding.die2,
-            binding.die3,
-            binding.die4,
-            binding.die5)
+                binding.die2,
+                binding.die3,
+                binding.die4,
+                binding.die5)
 
         binding.rollButton.setOnClickListener {
             rollTheDice()
@@ -76,15 +76,15 @@ class MainFragment : Fragment() {
 
     private fun rollTheDice() {
 
-        for (dieIndex in imageViews.indices){
+        for (dieIndex in imageViews.indices) {
 
-            thread (start = true) {
+            thread(start = true) {
                 Thread.sleep(dieIndex * 100L) //delaying starts so all dies look differently animating.
 
                 val bundle = Bundle()
                 bundle.putInt(DIE_INDEX_KEY, dieIndex)
 
-                for(i in 1..20) {
+                for (i in 1..20) {
                     val dieNumber = getDieValue()
                     bundle.putInt(DIE_VALUE_KEY, dieNumber)
                     Thread.sleep(100)
@@ -101,7 +101,7 @@ class MainFragment : Fragment() {
 
     }
 
-    private fun getDieValue() : Int {
+    private fun getDieValue(): Int {
         return Random.nextInt(1, 7) //until isn't inclusive.
     }
 
