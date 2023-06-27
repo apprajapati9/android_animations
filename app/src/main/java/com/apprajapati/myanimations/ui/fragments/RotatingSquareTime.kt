d5a2522a5427404727ec8fe230b1d4ec5bbccf2f
apprajapati <apprajapati@hotmail.com>
2023-06-26 22:14:28 -0400

    added rotating square indefinite timer to call getTime api, drag and drop functionality and handled error on no internet connection. Using Timer() to perform api call indefinitely until stopped every second.

--- app/src/main/java/com/apprajapati/myanimations/ui/fragments/RotatingSquareTime.kt
+++ app/src/main/java/com/apprajapati/myanimations/ui/fragments/RotatingSquareTime.kt
@@ -1,17 +1,35 @@
 package com.apprajapati.myanimations.ui.fragments
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.ClipData
+import android.content.ClipDescription
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Point
+import android.graphics.drawable.Drawable
+import android.health.connect.datatypes.HeightRecord
 import android.os.Bundle
+import android.view.DragEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.apprajapati.myanimations.R
 import com.apprajapati.myanimations.databinding.FragmentRotatingSquareTimeBinding
 import com.apprajapati.myanimations.util.NetworkResult
 import com.apprajapati.myanimations.util.getTimeOnly
 import com.apprajapati.myanimations.util.getTimeOnlyTimeApi
 import com.apprajapati.myanimations.util.rotateViewUsingObjectAnimator
 import com.apprajapati.myanimations.viewmodels.MainViewModel
+import com.google.android.material.dialog.InsetDialogOnTouchListener
 import java.util.Timer
 import java.util.TimerTask
 
@@ -23,7 +41,6 @@ class RotatingSquareTime : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    private var timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,68 +56,157 @@ class RotatingSquareTime : Fragment() {
 
 
         viewModel.time.observe(viewLifecycleOwner) { response ->
-            when(response) {
+            when (response) {
                 is NetworkResult.Success -> {
                     val time = response.data
                     binding.textViewTime.text = getTimeOnly(time?.datetime!!)
                 }
+
                 is NetworkResult.Error -> {
                     val message = response.message
-                    binding.textViewTime.text =  message
-                    timer.cancel()
+                    binding.textViewTime.text = message
+                    viewModel.stopTimer()
                 }
-                is NetworkResult.Loading -> binding.textViewTime.text =  "Loading"
+
+                is NetworkResult.Loading -> binding.textViewTime.text = "Loading"
             }
         }
 
-        viewModel.timeApi.observe(viewLifecycleOwner) {
-                response ->
-            when(response) {
+        viewModel.timeApi.observe(viewLifecycleOwner) { response ->
+            when (response) {
                 is NetworkResult.Success -> {
                     val time = response.data
                     binding.textViewTime.text = getTimeOnlyTimeApi(time?.dateTime!!)
                 }
+
                 is NetworkResult.Error -> {
                     val message = response.message
-                    binding.textViewTime.text =  message
-                    timer.cancel()
+                    binding.textViewTime.text = message
+                    viewModel.stopTimer()
                 }
-                is NetworkResult.Loading -> binding.textViewTime.text =  "Loading"
+
+                is NetworkResult.Loading -> binding.textViewTime.text = "Loading"
             }
         }
 
         val animator = rotateViewUsingObjectAnimator(binding.textViewTime)
         animator.start()
-        animator.end()
+        // animator.end()
 
-        binding.requestButton.setOnClickListener {
-            viewModel.getTime()
+//        binding.requestButton.setOnClickListener {
+//            viewModel.getTime()
+//        }
+        binding.requestButton.visibility = View.GONE
+
+        binding.textViewTime.setOnLongClickListener {
+            //val item = ClipData.Item("","")
+            val dragData = ClipData.newPlainText("", "")
+            val shadow = MyDragShadowBuilder(binding.textViewTime)
+
+            binding.textViewTime.startDragAndDrop(dragData, shadow, it, 0)
+            true
         }
 
+
+        binding.mainContainer.setOnDragListener(dragListener)
         return binding.root
+    }
+
+
+    val dragListener = View.OnDragListener { view, event ->
+
+        val draggedView = event.localState as View
+
+        when (event.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                binding.textViewTime.visibility = View.GONE
+                true
+            }
+
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                true
+            }
+
+            DragEvent.ACTION_DRAG_EXITED -> {
+                binding.textViewTime.visibility = View.VISIBLE
+                true
+            }
+
+            DragEvent.ACTION_DRAG_ENDED -> {
+                binding.textViewTime.visibility = View.VISIBLE
+                true
+            }
+
+            DragEvent.ACTION_DRAG_LOCATION -> {
+                true
+            }
+
+            DragEvent.ACTION_DROP -> {
+
+                draggedView.x = event.x - draggedView.width / 2
+                draggedView.y = event.y - draggedView.height / 2
+
+
+                true
+            }
+
+            else -> false
+        }
+
+    }
+
+
+    private class MyDragShadowBuilder(view: View) : View.DragShadowBuilder(view) {
+
+        private val shadow = ContextCompat.getDrawable(view.context, R.drawable.rotating_square_box)
+        private val mText = TextView(view.context)
+
+        init {
+            //mText.text = "Ajay"
+            //mText.background = shadow
+        }
+
+        //to get the size and position of the drag shadow. It uses this data to construct a canvas object. then it calls onDrawShadow() so that your application can drw the shadow image in the canvas.
+        override fun onProvideShadowMetrics(outShadowSize: Point?, outShadowTouchPoint: Point?) {
+            super.onProvideShadowMetrics(outShadowSize, outShadowTouchPoint)
+            //view.heigh/width gives the full size of original view.
+            val width = view.width
+            val height = view.height
+
+            shadow?.setBounds(0, 0, width, height)
+            //mText.background = shadow
+            outShadowSize?.set(width, height)
+            //mText.text = "Ajay"
+
+            outShadowTouchPoint?.set(width / 2, height / 2)
+        }
+
+        override fun onDrawShadow(canvas: Canvas) {
+            //shadow?.setBounds(0, 0, view.width, view.height)
+            //mText.background = shadow
+
+            //mText.layout(0,0, view.width, view.height)
+
+
+            shadow?.draw(canvas)
+        }
+
     }
 
     override fun onPause() {
         super.onPause()
-        timer.cancel()
+        viewModel.stopTimer()
     }
 
     override fun onResume() {
         super.onResume()
-        val task = object : TimerTask() {
-            override fun run() {
-                viewModel.getTime()
-            }
+        viewModel.startFetchingTime()
 
-        }
-        timer = Timer()
-        timer.scheduleAtFixedRate(task, 0, 1000)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        timer.cancel()
-        timer.purge()
+        viewModel.stopTimer()
         _binding = null
     }
 }
--- app/src/main/java/com/apprajapati/myanimations/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/apprajapati/myanimations/viewmodels/MainViewModel.kt
@@ -10,44 +10,77 @@ import com.apprajapati.myanimations.models.TimeApiTime
 import com.apprajapati.myanimations.util.NetworkResult
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.lang.Exception
+import java.util.Timer
+import java.util.TimerTask
 
 class MainViewModel : ViewModel() {
 
-    private val _time : MutableLiveData<NetworkResult<Time>> = MutableLiveData()
-    val time : LiveData<NetworkResult<Time>> get() = _time
+    private val _time: MutableLiveData<NetworkResult<Time>> = MutableLiveData()
+    val time: LiveData<NetworkResult<Time>> get() = _time
 
-    private val _timeApiTime : MutableLiveData<NetworkResult<TimeApiTime>> = MutableLiveData()
-    val timeApi : LiveData<NetworkResult<TimeApiTime>> get() = _timeApiTime
+    private val _timeApiTime: MutableLiveData<NetworkResult<TimeApiTime>> = MutableLiveData()
+    val timeApi: LiveData<NetworkResult<TimeApiTime>> get() = _timeApiTime
 
-    fun getTime(){
+    private var timer = Timer()
+
+    fun getTime() {
         viewModelScope.launch {
-            val gotTime = RetrofitInstance.api.getTime()
-            _time.value = handleResponse(gotTime)
+            try {
+                val gotTime = RetrofitInstance.api.getTime()
+                _time.value = handleResponse(gotTime)
+            }catch (e: Exception){
+                e.printStackTrace() //Occasions where internet connection will be lost so api.getTime() will be kept calling and failing so you must handle this exception and stop the thread.
+                _time.value = NetworkResult.Error("Error fetching time!")
+                stopTimer()
+            }
         }
     }
 
-    fun getTimeFromTimeApi(){
+    fun getTimeFromTimeApi() {
         viewModelScope.launch {
             val response = RetrofitInstance.api.getTimeTimeApi()
             _timeApiTime.value = handleResponseTimeApi(response)
         }
     }
 
-    private fun handleResponse(response: Response<Time>) : NetworkResult<Time> {
+    fun startFetchingTime() {
+        val task = object : TimerTask() {
+            override fun run() {
+                getTime()
+            }
+
+        }
+        timer = Timer()
+        timer.scheduleAtFixedRate(task, 0, 1000)
+        //delay = start the task after that much time has passed.
+        //period = run timer every 1 second, 1000 milliseconds=1 second.
+    }
+
+    fun stopTimer() {
+        timer.cancel()
+        timer.purge()
+    }
+
+    private fun handleResponse(response: Response<Time>): NetworkResult<Time> {
         when {
             response.message().toString().contains("timeout") -> {
                 return NetworkResult.Error("Timeout")
             }
+
             response.code() == 402 -> {
                 return NetworkResult.Error("API Key Limited!")
             }
+
             response.code() == 429 -> {
                 return NetworkResult.Error("Too Many Requests!")
             }
+
             response.isSuccessful -> {
                 val Time = response.body()
                 return NetworkResult.Success(Time!!)
             }
+
             else -> {
                 return NetworkResult.Error(response.message())
             }
@@ -55,21 +88,25 @@ class MainViewModel : ViewModel() {
 
     }
 
-    private fun handleResponseTimeApi(response: Response<TimeApiTime>) : NetworkResult<TimeApiTime> {
+    private fun handleResponseTimeApi(response: Response<TimeApiTime>): NetworkResult<TimeApiTime> {
         when {
             response.message().toString().contains("timeout") -> {
                 return NetworkResult.Error("Timeout")
             }
+
             response.code() == 402 -> {
                 return NetworkResult.Error("API Key Limited!")
             }
+
             response.code() == 429 -> {
                 return NetworkResult.Error("Too Many Requests!")
             }
+
             response.isSuccessful -> {
                 val Time = response.body()
                 return NetworkResult.Success(Time!!)
             }
+
             else -> {
                 return NetworkResult.Error(response.message())
             }
