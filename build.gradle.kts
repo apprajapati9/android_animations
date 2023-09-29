4eb729c35b6d9bb358dc13368ca5d9e8ed9f4904
apprajapati <apprajapati@hotmail.com>
2023-09-29 01:12:51 -0400

    added attrset for the custom loading view

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
         maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.1.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -20,6 +20,6 @@ plugins {
 //    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
 
-tasks.create<Delete>("clean"){
+tasks.create<Delete>("clean") {
     delete(rootProject.buildDir)
 }
--- customLoadingViews/src/main/java/com/apprajapati/loadingviews/CircularLoadingView.kt
+++ customLoadingViews/src/main/java/com/apprajapati/loadingviews/CircularLoadingView.kt
@@ -8,19 +8,21 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
 import android.graphics.RectF
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
 import android.view.animation.DecelerateInterpolator
+import androidx.core.content.ContextCompat
+import androidx.core.view.updateLayoutParams
 
-class CircularLoadingView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet), Runnable{
+class CircularLoadingView(context: Context, attributeSet: AttributeSet) :
+    View(context, attributeSet), Runnable {
 
-    private val centerXY= PointF(0f, 0f)
-    private val mCirclePaint = Paint().apply{
-        isAntiAlias = true
-        style = Paint.Style.STROKE //stroke will make one layer only, fill will draw a filled circle.
-        color = Color.BLACK
-        strokeWidth = 10f
-    }
+    private val centerXY = PointF(0f, 0f)
 
     var minHeight = 0f //Minimum height and width of the view.
         get() = field
@@ -34,50 +36,97 @@ class CircularLoadingView(context: Context, attributeSet: AttributeSet) : View(c
 
     var valueAnimator = ValueAnimator()
     var fadeAnimator = ValueAnimator()
+    var circleColor = Color.WHITE
+    var animationDuration = 1000
+    var circleWidth = 15f
+
+    private var mCirclePaint = Paint()
+    private var outerCirclePaint = Paint()
+    private var shownOuter = false
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    init {
+        val setValues =
+            context.obtainStyledAttributes(attributeSet, R.styleable.CircularLoadingView, 0, 0)
+        animationDuration =
+            setValues.getInteger(R.styleable.CircularLoadingView_duration, animationDuration)
+        // circleColor = setValues.getColor(R.styleable.CircularLoadingView_circleColor, Color.BLACK)
+        circleWidth = setValues.getFloat(R.styleable.CircularLoadingView_circleWidth, circleWidth)
+        setValues.recycle()
+
+        mCirclePaint = Paint()
+        mCirclePaint.isAntiAlias = true
+        mCirclePaint.style =
+            Paint.Style.STROKE //stroke will make one layer only, fill will draw a filled circle.
+        mCirclePaint.color = circleColor
+        mCirclePaint.strokeWidth = circleWidth
+
+        outerCirclePaint = Paint()
+        outerCirclePaint.isAntiAlias = true
+        outerCirclePaint.style =
+            Paint.Style.STROKE //stroke will make one layer only, fill will draw a filled circle.
+        outerCirclePaint.color = ContextCompat.getColor(context, R.color.black_fade)
+        outerCirclePaint.alpha = 100
+        outerCirclePaint.strokeWidth = circleWidth / 2
+    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         //centerXY.getCenterXY() //at this point in this method doesn't give the correct center points.
-        //viewRect.set(centerXY.x, centerXY.y, minHeight, minHeight)
         outerCircleRadius = minHeight
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         //Always pick the minimum value as a radius so full circle can be seen.
-        if(w < h){
-            minHeight = w.toFloat().div(2)-5
-        }else{
-            minHeight = h.toFloat().div(2)-5
+        if (w < h) {
+            minHeight = w.toFloat()
+                .div(2) - circleWidth / 2  //-circleWidth so it doesn't get cropped when width is too big.
+        } else {
+            minHeight = h.toFloat().div(2) - circleWidth / 2
         }
 
-        if(w == h){
-            minHeight = w.toFloat().div(2)-5
+        if (w == h) {
+            minHeight = w.toFloat().div(2) - circleWidth / 2
         }
-
-        mCirclePaint.strokeWidth = minHeight/8
+        centerXY.getCenterXY()
         animateProgress()
     }
 
     override fun onDraw(canvas: Canvas) {
-        centerXY.getCenterXY()
-
         canvas.drawCircle(centerXY.x, centerXY.y, radius, mCirclePaint)
+
+        if (shownOuter.not()) {
+            canvas.drawCircle(centerXY.x, centerXY.y, minHeight, outerCirclePaint)
+        }
     }
 
-    fun PointF.getCenterXY(){
+    fun PointF.getCenterXY() {
         x = width.toFloat().div(2) //width/2).toFloat()
         y = height.toFloat().div(2) //height/2).toFloat()
     }
 
-    fun animateProgress(){
-        val valuesHolder= PropertyValuesHolder.ofFloat("progressValue", 0f, minHeight)
+    private fun getColorRange(): PropertyValuesHolder {
+        if (circleColor == Color.BLACK) {
+            return PropertyValuesHolder.ofInt("colorValues", 0, 255)
+        } else {
+            return PropertyValuesHolder.ofInt("colorValues", 255, 210, 200, 0)
+        }
+    }
 
-        val colorValues = PropertyValuesHolder.ofInt("colorValues",  0, 255) //0 being black and 255 being white.
+    fun animateProgress() {
+        val valuesHolder = PropertyValuesHolder.ofFloat("progressValue", 0f, minHeight)
+
+        val colorValues = getColorRange() //0 being black and 255 being white.
+
+        handler.postDelayed({
+            shownOuter = true
+        }, 500)
 
         valueAnimator = ValueAnimator().apply {
             setValues(valuesHolder, colorValues)
-            duration = 1200
+            duration = animationDuration.toLong()
             repeatCount = ValueAnimator.INFINITE
 
             interpolator = DecelerateInterpolator(2f) //10f does give some interesting effects
@@ -101,6 +150,7 @@ class CircularLoadingView(context: Context, attributeSet: AttributeSet) : View(c
         super.onDetachedFromWindow()
         valueAnimator.end()
     }
+
     override fun run() {
         TODO("Not yet implemented")
     }
